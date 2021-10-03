9df7160827a23087de19ea3d852a1a98dfce7c8c
Divyansh Agarwal <77184239+nikkuAg@users.noreply.github.com>
2021-10-03 19:56:16 +0530

    Update settings.py

--- SOCE/settings.py
+++ SOCE/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7h_9&13-cc5hwyc)bsrxrs5u%k6@#hpf&cw^mn=g((8@3!)rzb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['soce-backend.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
