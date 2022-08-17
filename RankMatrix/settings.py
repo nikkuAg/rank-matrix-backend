af7def135e4008a2a5f16f5178d43d55297eabb5
Divyansh Agarwal <a.divyansh.25@gmail.com>
2022-08-17 19:02:34 +0530

    Create docker env file

--- RankMatrix/settings.py
+++ RankMatrix/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from config import details
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -133,7 +132,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
