a12d92fc2b66015497038f999a2d551645cb00d1
samratmiddha <s_middha@me.iitr.ac.in>
2023-06-05 00:05:23 +0530

    Fix ordering fields

--- rank_matrix/constants/order_fields.py
+++ rank_matrix/constants/order_fields.py
@@ -1,9 +1,9 @@
-COLLEGE_LIST_ORDER = ['code', 'name', 'state', 'nirf_1', 'nirf_2', 'nirf_3']
+COLLEGE_LIST_ORDER = ['code', 'name', 'state', 'nirf_1', 'nirf_2', 'nirf_3','website']
 
 
-SEAT_MATRIX_ORDER = ['quota__quota', 'category__category', 'seat_pool__seat_pool', 'seats', 'college_code__name', 
-	'college_code__display_code','branch_code__branch_name', 'branch_code__branch_code', 
+SEAT_MATRIX_ORDER = ['quota__quota', 'category__category', 'seat_pool__seat_pool', 'seats', 'institute_code__name', 
+	'institute_code__display_code','branch_code__branch_name', 'branch_code__branch_code', 
 	'branch_code__duration', 'branch_code__degree']
 
 OPENING_CLOSING_ORDER = ['quota__quota', 'category__category', 'seat_pool__seat_pool', 'opening_rank', 'closing_rank', 
-	'college_code__name', 'college_code__display_code','branch_code__branch_name', 'branch_code__branch_code']
+	'institute_code__name', 'institute_code__display_code','branch_code__branch_name', 'branch_code__branch_code']
